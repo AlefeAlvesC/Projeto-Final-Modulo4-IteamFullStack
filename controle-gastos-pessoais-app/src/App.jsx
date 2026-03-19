359408fa205a3e3a57dee296a34590f0d92ad298
Amanda <amandaufrr23@gmail.com>
2026-03-19 19:07:04 -0400

    criação dos componentes criação do footer importação das pastas e dos componentes

--- controle-gastos-pessoais-app/src/App.jsx
+++ controle-gastos-pessoais-app/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Routes, Route } from 'react-router-dom'
+import GastosContext from './contexts/GastosContext'
+import PaginaInicio from './pages/PaginaInicio'
+import PaginaGastos from './pages/PaginaGastos'
+import PaginaDetalhes from './pages/PaginaDetalhes'
+import PaginaNaoEncontrada from './pages/PaginaNaoEncontrada'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+    < Footer/>
+    </div>
   )
 }
 
---
+++ controle-gastos-pessoais-app/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return ( 
+    <footer>
+    <small>Projeto Final – Desenvolvimento Front-end com React</small>
+    </footer>
+    )
+}
+export default Footer
