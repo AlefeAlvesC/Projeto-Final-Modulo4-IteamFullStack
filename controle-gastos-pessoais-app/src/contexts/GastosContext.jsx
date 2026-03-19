bc099842dd75d70d0372000b7b83e980ea8d2487
Alefe <alefealvesdacosta@gmail.com>
2026-03-19 19:10:47 -0400

    Adicionando lógica inicial do Context para o gastos

--- controle-gastos-pessoais-app/src/contexts/GastosContext.jsx
+++ controle-gastos-pessoais-app/src/contexts/GastosContext.jsx
@@ -4,14 +4,30 @@ import { createContext, useContext , useState, useEffect} from "react";
 export const GastosContext = createContext(null);
 
 export const GastosProvider = ({children}) => {
-    conts [gastos, setGastos] = useState(() => {
+    const [gastos, setGastos] = useState(() => {
         const stored = localStorage.getItem('meus-gastos');
 
         if (!stored) return [
-            
+            {id: "1", nome: "Rancho" , categoria: "Comida", valor: "500", pago: false}
         ]
 
+        try{
+            return JSON.parse(stored);
+        }catch{
+            return [];
+        }
+
     });
+
+    useEffect(() => {
+        localStorage.setItem("meus-gastos", JSON.stringify(gastos));
+    }, [gastos])
+
+    return(
+        <GastosContext.Provider value={{gastos}}>
+            {children}
+        </GastosContext.Provider>
+    );
 } 
 
 
