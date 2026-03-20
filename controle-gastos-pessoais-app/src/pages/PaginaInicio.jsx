eb37cc8cb881605d146da088a9af037250efba1f
Alefe <alefealvesdacosta@gmail.com>
2026-03-20 06:48:51 -0400

    Adicionando lógica inicial para cadastro de um novo gasto via categorias utilizando Link para ligar as páginas

--- controle-gastos-pessoais-app/src/pages/PaginaInicio.jsx
+++ controle-gastos-pessoais-app/src/pages/PaginaInicio.jsx
@@ -2,6 +2,7 @@
 
 import { Link } from 'react-router-dom'
 import { useGastos } from '../contexts/GastosContext'
+import GastosSugests from '../components/GastosSugests'
 
 function PaginaInicio() {
     const { gastos } = useGastos()
@@ -11,13 +12,16 @@ function PaginaInicio() {
             <h1>Meus Gastos Pessoais</h1>
             <p>Seu painel de controle pessoal para gerenciar seus gastos financeiros.</p>
 
+            {/* 
             <div className="resumo">
                 <div className="resumo-card">
                     <strong>{gastos.length}</strong>
                     <span>Gastos Totais</span>
                 </div>
-
             </div>
+            */}
+            <GastosSugests />
+            
 
             <Link to="/lista-gasto" className="btn-primario" style={{ fontSize: '1.1rem', padding: '1rem 2.5rem' }}>
                 Ver meus gastos →
