45e0d52f2eae340cb8ba502e3672a5512f0e90b2
Amanda <amandaufrr23@gmail.com>
2026-03-20 21:41:43 -0400

    criaçao do cardInicio e sua estilização estilo page inicio e gastos e detalhes

--- controle-gastos-pessoais-app/src/pages/PaginaGastos.jsx
+++ controle-gastos-pessoais-app/src/pages/PaginaGastos.jsx
@@ -1,12 +1,15 @@
 // src/pages/PaginaGastos.jsx
 
 import GastosList from '../components/GastosList'
+import { useGastos } from '../contexts/GastosContext'
 
 function PaginaGastos() {
+    const {gastos} = useGastos()
     return (
         <main className="pagina-gastos container">
             <h1>Meus Gastos Pessoais</h1>
             <GastosList />
+            {gastos.length===0 && (<p>Nenhum Gasto Cadastrado</p>)}
         </main>
     )
 }
